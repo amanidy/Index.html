d3a16933be698b141f5ac5d083a0184b14eecdc9
Arnold Amani <arnoldamani08@gmail.com>
2024-02-16 15:38:55 +0300

    Added index.html

---
+++ index.js
@@ -0,0 +1,9 @@
+ // javascript object
+ /*const myObject = {
+    first: "Heyy Raymond!",
+    second: 456,
+    third:true
+}
+myObject.first = "Good to see you Dembe!!"; myObject["first"];
+console.log(myObject.first); */
+
